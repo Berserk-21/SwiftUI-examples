14ea9dfbd3edb32a44d353eed2ffed26f7608d80
Berserk-21 <cryptogekkofr@protonmail.com>
2024-08-13 10:14:49 +0200

    Presents menu section and items in a list structure

--- iDine/iDine/ContentView.swift
+++ iDine/iDine/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                // The model should inherit the Identifiable protocol and owns an id property.
+                // This helps SwiftUI identify and generate the cells of the list with the ForeEach.
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
         }
-        .padding()
     }
 }
 
