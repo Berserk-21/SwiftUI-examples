5e1985a27ef9dc363b8bce0e3bfad8b225381e3a
Berserk-21 <cryptogekkofr@protonmail.com>
2024-08-13 09:57:39 +0200

    initialized project

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Berserk on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
