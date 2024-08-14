772fd5bc77cbd5c84710761475c7bc4b5793bac3
Berserk-21 <cryptogekkofr@protonmail.com>
2024-08-14 10:35:16 +0200

    Initialized project

---
+++ StaticText/StaticText/StaticTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticTextApp.swift
+//  StaticText
+//
+//  Created by Berserk on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct StaticTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
